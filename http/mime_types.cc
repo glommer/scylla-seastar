5b6d85f0415aa667c2e9dd791b8fd5f1bf92f68b
Amnon Heiman <amnon@scylladb.com>
2017-08-21 15:39:16 +0300

    http: Add MIME type support for JSON
    JSON is used in the API and it should be mapped to 'application/json'
    MIME type. Currently, it is not mapped to anything and therefore the
    default text plain type is used.
    
    Signed-off-by: Amnon Heiman <amnon@scylladb.com>
    Message-Id: <20170821123916.5278-1-amnon@scylladb.com>

--- http/mime_types.cc
+++ http/mime_types.cc
@@ -17,6 +17,7 @@ struct mapping {
     const char* extension;
     const char* mime_type;
 } mappings[] = {
+        { "json", "application/json"},
         { "gif", "image/gif" },
         { "htm", "text/html" },
         { "css", "text/css" },
